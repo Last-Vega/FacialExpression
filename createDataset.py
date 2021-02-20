abdde752f55270a5201607fa61cd1e3e2c9dd53a
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-02-20 16:00:56 +0900

    Create createDataset.py

---
+++ createDataset.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+sub_dir = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
+result_dir = './results'
+result_subdir = './results/'
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+    for i in range(len(sub_dir)):
+        os.mkdir(result_subdir + '/' + sub_dir[i])
+
+for dir_name in sub_dir:
+    target_dir = './MMAFEDB/train/' + dir_name
+    fltr_list = [filename for filename in os.listdir(target_dir) if not filename.startswith('.')]
+
+    for i in range(50):
+        newImgName = dir_name + '_' + str(i)
+        shutil.copyfile(target_dir+'/'+fltr_list[i], './results/'+dir_name+'/'+newImgName+'.jpg')
