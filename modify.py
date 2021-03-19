3a2665d2159db77735f6765618c116f60a46ed34
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-03-19 12:05:27 +0900

    Update modify.py

--- modify.py
+++ modify.py
@@ -3,6 +3,8 @@ import sys
 import os
 import pandas as pd
 
-df = pd.read_csv('./Batch_4350992_batch_results.csv')
+# df = pd.read_csv('./Batch_4350992_batch_results.csv')
+df = pd.read_csv('./Batch_4369243_batch_results.csv')
 df = df[['HITId', 'WorkerId', 'Answer.category.label']]
-df.to_csv('./for_ds.csv', index=False)
+# df.to_csv('./for_ds.csv', index=False)
+df.to_csv('./for_ds-1.csv', index=False)
